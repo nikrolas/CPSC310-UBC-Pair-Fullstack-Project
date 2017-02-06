9e2eef094abf170910302caa35c975d9540f5cde
rt-chang <rchang21@gmail.com>
2017-02-05 17:34:09 -0800

    Refactored code to better handle 400 Broken empty WHERE implementation

--- test/InsightFacadeSpec.ts
+++ test/InsightFacadeSpec.ts
@@ -21,7 +21,7 @@ describe("InsightFacadeSpec", function () {
 
     it("Dataset didn't exist; added successfully", function () {
         fs.unlinkSync('./cache.json');
-       return insightFacade.addDataset("courses", data.toString('base64'))
+       return insightFacade.addDataset("courses", data.toString( 'base64'))
             .then(function (response) {
                 expect(response.code).is.equal(204);
             })
@@ -758,13 +758,14 @@ describe("InsightFacadeSpec", function () {
     it("Invalid dataset ID to be queried", function () {
         let qr: QueryRequest = {
             WHERE:{
-                EQ:{
-                    "invalid_pass": 9
+                GT:{
+                    "invalid1_avg":97
                 }
             },
             OPTIONS:{
                 COLUMNS:[
-                    "invalid_dept"
+                    "invalid2_dept",
+                    "invalid3_avg"
                 ],
                 FORM:"TABLE"
             }
@@ -893,12 +894,12 @@ describe("InsightFacadeSpec", function () {
             })
     });
 
-    it("WHERE empty test", function () {
+    it("WHERE valid empty test", function () {
         let qr: QueryRequest = {
             WHERE:{
             },
             OPTIONS:{
-                "COLUMNS":[
+                COLUMNS:[
                     "courses_dept",
                     "courses_avg"
                 ],
@@ -908,10 +909,10 @@ describe("InsightFacadeSpec", function () {
         };
         insightFacade.performQuery(qr)
             .then(function (response) {
-                expect.fail();
+                expect(response.code).is.equal(200);
             })
             .catch(function (err) {
-                expect(err.code).is.equal(400);
+                expect.fail();
             })
     });
 
@@ -951,27 +952,17 @@ describe("InsightFacadeSpec", function () {
     it("NOT test", function () {
         let qr : QueryRequest = {
             WHERE: {
-                AND: [
-                    {
-                        GT:{
-                            "courses_avg":61
-                        }
-                    },
-                    {
-                        LT:{
-                            "courses_avg":62
-                        }
-                    },
-                    {
+
+
+
                         NOT:{
 
                                 IS: {
-                                    "courses_dept":"apbi"
+                                    "courses_dept":"aanb"
                                 }
 
                         }
-                    }
-                ]
+
             },
             OPTIONS: {
                 COLUMNS: [
@@ -1107,88 +1098,4 @@ describe("InsightFacadeSpec", function () {
                 expect.fail();
             })
     });
-
-    // it.only("remove dataset that is not in the set, error thrown", function () {
-    //     insightFacade.removeDataset("blah")
-    //         .then(function (response) {
-    //             console.log("In remove test");
-    //             expect.fail();
-    //         })
-    //         .catch(function (err) {
-    //             console.log("In remove test");
-    //             expect(err.code).is.equal(424);
-    //         })
-    // });
-    //
-    // it.only("remove dataset that is in the set", function () {
-    //     insightFacade.removeDataset("meow")
-    //         .then(function (response) {
-    //             console.log("In remove test");
-    //             expect(response.code).is.equal(200);
-    //         })
-    //         .catch(function (err) {
-    //             console.log("In remove test");
-    //             expect.fail();
-    //         })
-    // });
-    //
-    // it.only("Dataset didn't exist; added successfully", function () {
-    //     var data = fs.readFileSync("./test.zip");
-    //     insightFacade.addDataset("meow", data.toString('base64'))
-    //         .then(function (response) {
-    //             console.log("First test complete");
-    //             expect(response.code).is.equal(204);
-    //         })
-    //         .catch(function (err) {
-    //             console.log("fail 1");
-    //             expect.fail();
-    //         })
-    // });
-
-
-    // it.only("Perform Query; added successfully", function (done) {
-    //     insightFacade.performQuery(qr)
-    //         .then(function (response) {
-    //             console.log("First test complete");
-    //             expect(response.code).is.equal(204);
-    //             done();
-    //         })
-    //         .catch(function (err) {
-    //             console.log("fail 1");
-    //             expect.fail();
-    //             done();
-    //         })
-    // });
-
-    // it.only("Adding second dataset; added successfully", function (done) {
-    //     console.log("In second test");
-    //     var data = fs.readFileSync("./test2.zip").toString('base64');
-    //     insightFacade.addDataset("courses2", data)
-    //         .then(function (response) {
-    //             expect(response.code).is.equal(201);
-    //             console.log("Second test complete");
-    //             done();
-    //         })
-    //         .catch(function (err) {
-    //             console.log("fail 2-2");
-    //             expect.fail();
-    //             done();
-    //         })
-    // });
-
-    // it.only("Adding overriding dataset; added successfully", function () {
-    //     console.log("In second test");
-    //     var data = fs.readFileSync("./test3.zip").toString('base64');
-    //     return insightFacade.addDataset("meow", data)
-    //         .then(function (response) {
-    //             expect(response.code).is.equal(201);
-    //             console.log("Second test complete");
-    //         })
-    //         .catch(function (err) {
-    //             console.log(err);
-    //             expect.fail();
-    //         })
-    // });
-
-
 });
