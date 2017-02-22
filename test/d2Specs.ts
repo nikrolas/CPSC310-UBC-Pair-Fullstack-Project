bfd111a6d7376d8e5e036b61d7b59f5cca9bd8aa
rt-chang <rchang21@gmail.com>
2017-02-21 16:07:04 -0800

    Updated package.json

--- test/d2Specs.ts
+++ test/d2Specs.ts
@@ -7,7 +7,7 @@ import {expect} from 'chai';
 import InsightFacade from "../src/controller/InsightFacade";
 import {QueryRequest} from "../src/controller/IInsightFacade";
 
-describe.only("d2Spec", function () {
+describe("d2Spec", function () {
 
     var insightFacade: InsightFacade = null;
     var fs = require("fs");
