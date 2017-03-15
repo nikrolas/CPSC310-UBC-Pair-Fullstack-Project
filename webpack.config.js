e4fb23ba24d06577bbf7942f58b368b0569b6d12
Nicholas Chan <nicholas.s.chan@gmail.com>
2017-03-14 18:50:06 -0700

    Looks like react is working.

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Nicky on 2017-03-14.
+ */
+
+module.exports = {
+    entry: './src/rest/app/component/main.js',
+    output: {filename: './src/rest/public/bundle.js' },
+
+    module: {
+        loaders: [
+            {
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node-modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    }
+}
